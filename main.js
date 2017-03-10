c7fd3a1372900ef4751c5841e8f3a6996430c03f
Jonas Luebbers <jluebbers@squarespace.com>
2017-03-09 21:54:57 -0500

    Change colors with palette selector

--- main.js
+++ main.js
@@ -1,21 +1,47 @@
 const vertexPositions = [
-	-1, -1, 0,
-	-1, 1, 0,
-	1, -1, 0,
-	1, 1, 0
+  -1, -1, 0,
+  -1, 1, 0,
+  1, -1, 0,
+  1, 1, 0
 ]
 
 const minSize = 50
 const viscosity = 10
-
 const colorCount = 4
-const backgroundColor = [0.59, 0.05, 0.07] 
-const colors = [
-  1.00, 0.96, 0.91,
-  0.10, 0.22, 0.66,
-  0.05, 0.13, 0.31,
-  0.89, 0.75, 0.33
-]
+
+const backgroundColor = [0.59, 0.05, 0.07]
+const colors = {
+  "palette-1": [
+    1.00, 0.96, 0.91,
+    0.10, 0.22, 0.66,
+    0.05, 0.13, 0.31,
+    0.89, 0.75, 0.33
+  ],
+  "palette-2": [
+    0.00, 0.76, 0.79,
+    0.69, 0.05, 0.67,
+    1.00, 1.00, 0.00,
+    0.18, 0.07, 0.49
+  ],
+  "palette-3": [
+    1.00, 0.96, 0.91,
+    0.10, 0.22, 0.66,
+    0.05, 0.13, 0.31,
+    0.89, 0.75, 0.33
+  ],
+  "palette-4": [
+    1.00, 0.96, 0.91,
+    0.10, 0.22, 0.66,
+    0.05, 0.13, 0.31,
+    0.89, 0.75, 0.33
+  ],
+  "palette-5": [
+    1.00, 0.96, 0.91,
+    0.10, 0.22, 0.66,
+    0.05, 0.13, 0.31,
+    0.89, 0.75, 0.33
+  ]
+}
 
 const types = {
   "pattern-drop": 0,
@@ -41,22 +67,26 @@ class Operation {
 }
 
 function loadShader(gl, source, type) {
-	const shader = gl.createShader(type);
-	gl.shaderSource(shader, source);
+  const shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
   gl.compileShader(shader);
-  
-	if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-		console.error("Shader compilation failed." + gl.getShaderInfoLog(shader));
+
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    console.error("Shader compilation failed." + gl.getShaderInfoLog(shader));
     gl.deleteShader(shader);
-    
-  	return null;
+
+    return null;
   }
-  
-	return shader;
+
+  return shader;
 }
 
 function length(x, y) {
-    return Math.sqrt((x * x) + (y * y)) 
+  return Math.sqrt((x * x) + (y * y))
+}
+
+function getSelectedButtonId(buttons) {
+  return buttons.find(button => button.checked).id
 }
 
 function getPositionInCanvas(canvas, x, y) {
@@ -67,18 +97,18 @@ function getPositionInCanvas(canvas, x, y) {
 }
 
 require(['domReady!', 'text!vertex.glsl', 'text!fragment.glsl'], (document, vertexSource, fragmentSource) => {
-  const patterns = document.getElementById('patterns')
-  const patternButtons = Array.from(patterns.getElementsByClassName('radio-button'))
-  const getSelectedPattern = () => {
-    return patternButtons.find(button => button.checked === true)
-  }
-
-  const cursor = document.getElementById('cursor')
-  const cursorGraphics = Array.from(cursor.getElementsByTagName('svg'))
-  
   const canvas = document.getElementById('canvas')
   canvas.width = 800
   canvas.height = 600
+
+  const patterns = document.getElementById('patterns')
+  const patternButtons = Array.from(patterns.getElementsByClassName('radio-button'))
+
+  const palettes = document.getElementById('palettes')
+  const paletteButtons = Array.from(palettes.getElementsByClassName('radio-button'))
+
+  const cursor = document.getElementById('cursor')
+  const cursorGraphics = Array.from(cursor.getElementsByTagName('svg'))
 
   let startX = 0
   let startY = 0
@@ -87,7 +117,7 @@ require(['domReady!', 'text!vertex.glsl', 'text!fragment.glsl'], (document, vert
   canvas.addEventListener('mousedown', (e) => {
     if (e.button !== 0) { return }
 
-    const pattern = getSelectedPattern().id    
+    const pattern = getSelectedButtonId(patternButtons)
     cursorGraphics.forEach((graphic) => {
       if (graphic.dataset.pattern === pattern) {
         graphic.classList.add('is-visible')
@@ -125,7 +155,7 @@ require(['domReady!', 'text!vertex.glsl', 'text!fragment.glsl'], (document, vert
     isDragging = false
     lastOperationScale = 0
 
-    if (getSelectedPattern().id === "pattern-spray") {
+    if (getSelectedButtonId(patternButtons) === "pattern-spray") {
       const dx = e.clientX - startX
       const dy = e.clientY - startY
       const l = length(dx, dy)
@@ -141,7 +171,7 @@ require(['domReady!', 'text!vertex.glsl', 'text!fragment.glsl'], (document, vert
         const p1 = [x, y]
         const p2 = [x, y + dropRadius]
         const type = types["pattern-drop"]
-        
+
         const operation = new Operation(p1, p2, dropColor, type)
         operations.unshift(operation)
       }
@@ -151,37 +181,37 @@ require(['domReady!', 'text!vertex.glsl', 'text!fragment.glsl'], (document, vert
       const p2 = getPositionInCanvas(canvas, e.clientX, e.clientY)
 
       const color = Math.floor(Math.random() * colorCount)
-      const type = types[getSelectedPattern().id]
+      const type = types[getSelectedButtonId(patternButtons)]
 
       const operation = new Operation(p1, p2, color, type)
       operations.unshift(operation)
-    }  
+    }
   })
 
-	let gl = null
-	gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl")
-	if (!gl) {
-		alert("Unable to initialize WebGL. Maybe your browser doesn't support it.")
-		return
+  let gl = null
+  gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl")
+  if (!gl) {
+    alert("Unable to initialize WebGL. Maybe your browser doesn't support it.")
+    return
   }
-  
+
   gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight)
 
-	const vertexShader = loadShader(gl, vertexSource, gl.VERTEX_SHADER)
-	const fragmentShader = loadShader(gl, fragmentSource, gl.FRAGMENT_SHADER)
+  const vertexShader = loadShader(gl, vertexSource, gl.VERTEX_SHADER)
+  const fragmentShader = loadShader(gl, fragmentSource, gl.FRAGMENT_SHADER)
 
-	program = gl.createProgram()
-	gl.attachShader(program, vertexShader)
-	gl.attachShader(program, fragmentShader)
-	gl.linkProgram(program)
+  program = gl.createProgram()
+  gl.attachShader(program, vertexShader)
+  gl.attachShader(program, fragmentShader)
+  gl.linkProgram(program)
 
-	if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-		console.error('Unable to initialize the shader program: ' + gl.getProgramInfoLog(program))
-		return
-	}
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    console.error('Unable to initialize the shader program: ' + gl.getProgramInfoLog(program))
+    return
+  }
 
-	gl.useProgram(program)
-	gl.clearColor(backgroundColor[0], backgroundColor[1], backgroundColor[2], 1)
+  gl.useProgram(program)
+  gl.clearColor(backgroundColor[0], backgroundColor[1], backgroundColor[2], 1)
 
   const vertexPositionAttribute = gl.getAttribLocation(program, 'vertexPosition')
   const resolutionUniform = gl.getUniformLocation(program, 'resolution')
@@ -193,20 +223,21 @@ require(['domReady!', 'text!vertex.glsl', 'text!fragment.glsl'], (document, vert
   const operationCoordinatesUniform = gl.getUniformLocation(program, 'operationCoordinates')
   const lastOperationScaleUniform = gl.getUniformLocation(program, 'lastOperationScale')
 
-	const vertexPositionBuffer = gl.createBuffer()
-	gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer)
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexPositions), gl.STATIC_DRAW)
+  const vertexPositionBuffer = gl.createBuffer()
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer)
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexPositions), gl.STATIC_DRAW)
 
-	function draw() {
+  function draw() {
     gl.clear(gl.COLOR_BUFFER_BIT)
-    
+
     if (operations.length > 0) {
       const operationTypes = operations.map(op => op.type)
       const operationColors = operations.map(op => op.color)
       const operationCoordinates = operations.map(op => op.coordinates()).reduce((acc, val) => acc.concat(val))
+      const selectedColors = colors[getSelectedButtonId(paletteButtons)]
 
       gl.uniform2f(resolutionUniform, canvas.width, canvas.height)
-      gl.uniform3fv(colorsUniform, colors)
+      gl.uniform3fv(colorsUniform, selectedColors)
       gl.uniform3f(backgroundColorUniform, backgroundColor[0], backgroundColor[1], backgroundColor[2])
       gl.uniform1i(operationCountUniform, operations.length)
       gl.uniform1iv(operationTypesUniform, operationTypes)
@@ -220,10 +251,10 @@ require(['domReady!', 'text!vertex.glsl', 'text!fragment.glsl'], (document, vert
       gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4)
 
       lastOperationScale += (1 - lastOperationScale) / viscosity
-    }  
+    }
 
-		requestAnimationFrame(draw)
-	}
+    requestAnimationFrame(draw)
+  }
 
-	draw();
+  draw();
 })
